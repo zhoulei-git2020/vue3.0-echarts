cc0641a2d87e497dbef60b9e931dc0dc3f243ae7
周磊 <5502655+zhoulei19930524@user.noreply.gitee.com>
2020-12-11 14:02:00 +0800

    引入element和echarts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+      <router-view />
   </div>
 </template>
 
-<style lang="scss">
-html,body,#app{
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html,body,#app {
   width: 100%;
   height: 100%;
-  margin: 0;
   padding: 0;
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import  ECharts from 'echarts'
 
 Vue.config.productionTip = false
+
+//将echarts加入原型，以后的项目不需要import直接引用$echarts
+Vue.prototype.$echarts = ECharts 
 
 new Vue({
   router,
