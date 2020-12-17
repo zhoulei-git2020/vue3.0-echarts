4a4b4d3b048ff1a37fe920d30ac073a0a17b63fe
zhoulei-git2020 <812515692@qq.com>
2020-12-17 21:44:31 +0800

    销售趋势组件容器头部开发

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -11,6 +11,28 @@
                   <el-menu-item index="1">销售额</el-menu-item>
                   <el-menu-item index="2">访问量</el-menu-item>
                 </el-menu>
+                <div class="menu-right">
+                    <el-radio-group v-model="radioSelect" size="small">
+                      <el-radio-button label="今日"></el-radio-button>
+                      <el-radio-button label="本周"></el-radio-button>
+                      <el-radio-button label="本月"></el-radio-button>
+                      <el-radio-button label="本年"></el-radio-button>
+                    </el-radio-group>
+                    <el-date-picker
+                      type='daterange'
+                      v-model="date"
+                      range-separator="至"
+                      start-placeholder="开始日期"
+                      end-placeholder="结束日期"
+                      size="small"
+                      :picker-options="pickerOptions"
+                      unlink-panels
+
+                    />
+
+                   
+                    
+                </div>
             </div>
         </template>
      </el-card>
@@ -20,7 +42,37 @@
 export default {
   data(){
     return{
-      activeIndex:'1'
+      activeIndex:'1', //默认的索引
+      radioSelect:'今日',//默认的选项卡
+      date:'null',
+      pickerOptions:{
+        shortcuts:[{
+            text:'最近一周',
+            onClick(picker){
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600*24*1000*7)
+              picker.$emit('pick',[start,end])
+            }
+          },{
+            text:'最近一月',
+            onClick(picker){
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600*24*1000*30)
+              picker.$emit('pick',[start,end])
+            }
+          },{
+            text:'最近三个月',
+            onClick(picker){
+               const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600*24*1000*90)
+              picker.$emit('pick',[start,end])
+            }
+          }
+        ]
+      }
     }
   },
   methods:{
@@ -33,5 +85,9 @@ export default {
 <style lang="scss" scoped>
     .sales-view{
       margin-top: 20px;
+      .menu-wrapper{
+        display: flex;
+
+      }
     }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Card,Row,Col,Menu,MenuItem,RadioGroup,Radio,DatePicker} from 'element-ui'
+import { Card,Row,Col,Menu,MenuItem,RadioGroup,RadioButton,DatePicker} from 'element-ui'
 
 Vue.use(Card) //使用card组件
 Vue.use(Row) //使用card组件
@@ -7,5 +7,5 @@ Vue.use(Col) //使用card组件
 Vue.use(Menu) //使用Menu组件
 Vue.use(MenuItem) //使用MenuItem组件
 Vue.use(RadioGroup)//使用RadioGroup组件
-Vue.use(Radio)//使用Radio组件
+Vue.use(RadioButton)//使用Radio组件
 Vue.use(DatePicker)//使用DatePicker组件
