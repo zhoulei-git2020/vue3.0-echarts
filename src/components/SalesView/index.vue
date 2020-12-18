559fd5091ea929d47a3c455223ada4c75f1dcb9f
周磊 <5502655+zhoulei19930524@user.noreply.gitee.com>
2020-12-18 16:57:26 +0800

    销售额图表样式完成

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="sales-view">
-     <el-card shadow='hover'>
+     <el-card shadow='hover' :body-style="{padding:'0 0 20px 0'}">
         <template v-slot:header> 
             <div class="menu-wrapper">
                 <el-menu
                 mode="horizontal"
                 :default-active="activeIndex"
                 @select="onMenuSelect"
+                class="sales-view-menu"
                 >
                   <el-menu-item index="1">销售额</el-menu-item>
                   <el-menu-item index="2">访问量</el-menu-item>
@@ -27,14 +28,28 @@
                       size="small"
                       :picker-options="pickerOptions"
                       unlink-panels
+                      class="sales-view-data-picker"
 
-                    />
-
-                   
-                    
+                    />                                      
                 </div>
             </div>
         </template>
+        <div class="sales-view-chart-wrapper">
+           <v-chart 
+            :options="chartOption"></v-chart>
+           <div class="sales-view-list">
+             <div class="sales-view-title">排行榜</div>
+             <div class="list-item-wrapper">
+               <div class="list-item" v-for="item in ranData" :key="item.no">
+               <div 
+               :class="['list-item-no' , +item.no<=3 ? 'top-no' : '']">{{item.no}}</div>
+               <div class="list-item-name">{{item.name}}</div>
+               <div class="list-item-money">{{item.money}}</div>
+             </div>
+             </div>
+
+           </div>
+        </div>
      </el-card>
   </div>
 </template>
@@ -72,7 +87,94 @@ export default {
             }
           }
         ]
-      }
+      },
+      chartOption:{
+          title:{
+            text:"年度销售额",
+            textStyle:{
+              fontSize:12,
+              color:'#666'
+            },
+            left:25,
+            top:20
+          },
+          xAxis:{
+            type:'category',
+            data:['1月', '2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+            axisLine:{
+              lineStyle:{
+                color:'#999'
+              }
+            },
+            axisLabel:{
+              color:'#333'
+            },
+            axisTick:{
+             alignWithLabel:true,
+             lineStyle:{
+               color:'#999'
+             }
+            }
+          },
+          yAxis:{
+            axisLine:{
+              show:false
+            },
+            axisTick:{
+              show:false
+            },
+            splitLine:{
+              lineStyle:{
+                type:'dotted',
+                color:'#eee'
+              }
+            }
+          },
+          series:[{
+            type:'bar',
+            barWidth:'35%',
+            data:[200, 120, 190, 276, 654, 863, 932, 354, 765, 187, 154, 542, ],
+            color:['#3398DB'],
+            grid:{
+                top:70,
+                left:60,
+                right:60,
+                bottom:50
+
+
+
+            }
+
+
+          }]
+      },
+      ranData:[
+        {
+        no:1,
+        name:'麦当劳',
+        money:'232,232'
+        },
+        {
+        no:2,
+        name:'麦当劳',
+        money:'232,232'
+        },
+        {
+        no:3,
+        name:'麦当劳',
+        money:'232,232'
+        },
+        {
+        no:4,
+        name:'麦当劳',
+        money:'232,232'
+        },
+        {
+        no:5,
+        name:'麦当劳',
+        money:'232,232'
+        },
+      ]
     }
   },
   methods:{
@@ -86,8 +188,90 @@ export default {
     .sales-view{
       margin-top: 20px;
       .menu-wrapper{
+        position: relative; //允许浮动
         display: flex;
+        .sales-view-menu{
+           width: 100%;
+           padding-left: 20px;
+           .el-menu-item{
+             height: 50px;
+             line-height: 50px;
+             margin: 0 20px;
 
+           }
+        }
+        .menu-right{
+          position: absolute;
+          top: 0;
+          right: 20px; 
+          height: 50px;
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          .sales-view-data-picker{
+            margin-left: 20px;
+          }
+        }
+
+      }
+      .sales-view-chart-wrapper{
+        
+        display: flex;
+        height: 270px;
+    
+        .echarts{
+          flex: 0 0 75%;
+          width: 70%;
+          height: 100%;
+        }
+        .sales-view-list{
+          flex: 1;
+          width: 100%;
+          height: 100%;
+          overflow: hidden;
+          margin-top: 15px;
+        
+        .sales-view-title{
+          margin-top: 20px;
+          font-size: 17px;  
+          color: #666;
+          font-weight: 500px;
+        }
+        .list-item-wrapper{
+          margin-top: 15px;
+            .list-item{
+          display: flex;
+          align-items: center;  
+          font-size: 15px;
+          height: 20px;
+          padding:6px 20px 6px 0;
+            .list-item-no{
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              width: 20px;
+              height: 20px; 
+              color:#333 ;
+              }
+              &.top-no{
+                background: #000;
+                border-radius: 50%;
+                color: #fff;
+                font-weight: 500;
+              }
+              .list-item-name{
+                margin-left: 10px;
+                color:#333 ;
+              }
+              .list-item-money{
+                flex: 1;
+                text-align: right;
+              }
+            
+            
+          }
+        }      
+       }
       }
     }
 </style>
