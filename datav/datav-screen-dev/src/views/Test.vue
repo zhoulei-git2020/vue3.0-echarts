0a9d096a9508e3f47abffa22c415fad8644aa1aa
周磊 <5502655+zhoulei19930524@user.noreply.gitee.com>
2021-01-19 18:08:47 +0800

    timeline学习

--- datav/datav-screen-dev/src/views/Test.vue
+++ datav/datav-screen-dev/src/views/Test.vue
@@ -12,10 +12,60 @@ export default {
  setup(){
     const options = ref({})
     const update = () =>{
-      options.value = {
-        baseOptions:{},
-        options:[]
-      }
+      // options.value = {
+      //   baseOption:{
+      //       timeline:{
+      //         axisType:'category', //时间轴类型
+
+      //         //播放按钮样式
+      //         controlStyle:{
+      //           position:'left',
+               
+      //         },
+      //         //动画播放属性
+      //         autoPlay:true, //自动播放
+      //         playInterval:1000,//自动播放时间间隔
+      //         loop:false, //是否循环播放
+      //         inverse:false, //是否倒放 
+      //         currentIndex:1, //从哪个位置开始播放
+      //         realtime:true, //点击节点是否拖动视图
+              
+
+      //          //时间轴每个节点的数据
+      //         data:[  
+      //            {
+      //              value:'2002-01-01', //节点数据
+      //              symbol:'diamond',     //节点形状
+      //              symbolSize:20        //节点形状大小
+      //            },          
+      //            {
+      //              value:'2003-01-01',
+      //              symbol:'diamond'
+      //            }, 
+      //            {
+      //              value:'2004-01-01',
+      //              symbol:'diamond'
+      //            },     
+      //            {
+      //              value:'2005-01-01',
+      //              symbol:'diamond'
+      //            },     
+      //            {
+      //              value:'2006-01-01',
+      //              symbol:'triangle',
+      //              symbolSize:20
+      //            },         
+                
+      //           ],
+      //           label:{
+      //             formatter(v){
+      //               return new Date(v).getFullYear()
+      //             }
+      //           }  
+      //       }
+      //   },
+      //   options:[]
+      // }
     }
     onMounted(update)
     return{
